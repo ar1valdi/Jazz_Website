454bc19ee514bae13d6076195e9685a9d024182c
ar1valdi <127351075+ar1valdi@users.noreply.github.com>
2023-03-24 00:02:51 +0100

    Add files via upload

---
+++ Version 2 - JS/script.js
@@ -0,0 +1,79 @@
+﻿window.onload = function () {
+    var button = document.createElement('button');
+    button.type = 'button';
+    button.onclick = przywroc;
+    var text = document.createTextNode('Przywróć');
+    button.appendChild(text);
+    document.getElementById('nowyGuzik').appendChild(button);
+}
+
+function changeColor() {
+    var thingToChange = document.getElementById('zmianaKoloru');
+    thingToChange.style.color = 'lightgrey';
+}
+
+function changeColorBack() {
+    var thingToChange = document.getElementById('zmianaKoloru');
+    thingToChange.style.color = 'white';
+}
+
+function zapamietaj() {
+    if (window.sessionStorage) {
+        var sprawdz = document.querySelector('input[name="plec"]:checked');
+
+        if (sprawdz != null) {
+            var formData = {
+                'imie': document.getElementById('imie').value,
+                'nazwisko': document.getElementById("nazwisko").value,
+                'plec': document.querySelector('input[name="plec"]:checked').value,
+                'poziom': document.getElementById("stan").value,
+                'email': document.getElementById("email").value,
+                'telefon': document.getElementById("tel").value,
+                'plik': document.getElementById("plik").value,
+                'msg': document.getElementById("msg").value
+            }
+
+            toJson = JSON.stringify(formData);
+            sessionStorage.dane = toJson;
+            console.log(sessionStorage.dane);
+        }
+        else {
+            var formData = {
+                'imie': document.getElementById('imie').value,
+                'nazwisko': document.getElementById("nazwisko").value,
+                'poziom': document.getElementById("stan").value,
+                'email': document.getElementById("email").value,
+                'telefon': document.getElementById("tel").value,
+                'plik': document.getElementById("plik").value,
+                'msg': document.getElementById("msg").value
+            }
+
+            toJson = JSON.stringify(formData);
+            sessionStorage.dane = toJson;
+            console.log(sessionStorage.dane);
+        }
+    }
+}
+
+function przywroc() {
+    if (window.sessionStorage && sessionStorage.dane) {
+        var formData = JSON.parse(sessionStorage.dane);
+        document.getElementById('imie').value = formData.imie;
+        document.getElementById('nazwisko').value = formData.nazwisko;
+
+        var plecRadio = document.querySelectorAll('input[name="plec"]');
+        for (let i = 0; i < plecRadio.length; i++) {
+            if (plecRadio[i].value === formData.plec)
+                plecRadio[i].checked = true;
+        }
+
+        document.getElementById('stan').value = formData.poziom;
+        document.getElementById('email').value = formData.email;
+        document.getElementById('tel').value = formData.telefon;
+        document.getElementById('plik').value = formData.plik;
+        document.getElementById('msg').value = formData.msg;
+    }
+}
+
+
+window.onunload = zapamietaj;
---
+++ Version 2 - JS/script_index.js
@@ -0,0 +1,16 @@
+﻿window.onload = function() {
+    if (window.localStorage) {
+        var licznik = document.getElementById("licznik");
+
+        if (licznik != null)
+            licznik.style.display = 'block';
+
+        if (localStorage.odwiedziny)
+            localStorage.odwiedziny = Number(localStorage.odwiedziny) + 1;
+        else
+            localStorage.odwiedziny = 1;
+
+        var text = document.createTextNode("Odwiedziłeś tę stronę " + localStorage.odwiedziny + " razy");
+        document.getElementById('tuWpisz').appendChild(text);
+    }
+}
